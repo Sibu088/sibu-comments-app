c4429b7114caf6e18847be1c0b977a928d0007d9
Sibu088 <mavismadira6@gmail.com>
2025-07-17 10:40:13 +0200

    Delete SecretToken.js

--- SecretToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "2y", // token expires in 2 years
-  });
-};
-
-module.exports = generateToken;
