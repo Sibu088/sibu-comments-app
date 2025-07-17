ec012897399a46849551c1ede2d9a5cb7170382e
Sibu088 <mavismadira6@gmail.com>
2025-07-17 10:40:38 +0200

    Delete Comment.js

--- Comment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const commentSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Comment", commentSchema);
